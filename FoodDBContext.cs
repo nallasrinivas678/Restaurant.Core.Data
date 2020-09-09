11f556d4df12ec9aaf75a1d75df4adb2cf98afb8
Srinivas nalla <nallasrinivas678@outlook.com>
2020-09-09 19:58:04 -0400

    Add project files.

---
+++ FoodDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Service = Restaurant.Core.Services;
+
+namespace Restaurant.Core.Data
+{
+    public class FoodDBContext: DbContext
+    {
+        public DbSet<Service.Restaurant> Restaurants { get; set; } 
+    }
+}
